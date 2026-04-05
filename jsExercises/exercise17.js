7bd9b56a411efedf8f9fc9efcf76958445dc908f
xanifaibraahim <xaniifoosman@gmail.com>
2026-04-05 04:23:00 +0200

    Add files via upload

---
+++ jsExercises/exercise17.js
@@ -0,0 +1,30 @@
+
+//variable temprature
+
+
+let temprature = 30;
+
+if (temprature < 0) {
+    console.log("very cold");
+
+
+}
+
+else if (temprature > 0 && temprature <= 15) {
+
+    console.log("cold");
+}
+
+else if (temprature > 15 && temprature <= 25) {
+
+    console.log("warm");
+
+
+}
+
+else if (temprature > 25) {
+
+    console.log("hot")
+
+
+}
