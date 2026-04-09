95c5adf157e45885b299ae66180d2f9cf565562a
xanifaibraahim <xaniifoosman@gmail.com>
2026-04-09 09:31:04 +0200

    Add files via upload

---
+++ jsExercises/exercise20.js
@@ -0,0 +1,16 @@
+
+
+
+let fruits = ["apple", "Banana", "cherry", "Strawberry"]
+
+
+let newArray = fruits.map(function (fruitvalue) {
+
+    return fruitvalue.length
+
+
+
+})
+
+
+console.log(newArray)
