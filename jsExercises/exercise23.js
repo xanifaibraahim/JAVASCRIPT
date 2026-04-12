d452628e1721665fdf0a47e341c065a18d54bf8e
xanifaibraahim <xaniifoosman@gmail.com>
2026-04-12 02:51:52 +0200

    Add files via upload

---
+++ jsExercises/exercise23.js
@@ -0,0 +1,18 @@
+
+
+let car = {
+
+
+    make: "Toyatta",
+    model: "Corolla",
+    year: 2020
+}
+
+
+//extract the make and model using object destructuring
+
+
+let { make, model } = car
+
+console.log(make)
+console.log(model)
