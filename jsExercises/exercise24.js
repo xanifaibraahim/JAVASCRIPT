9e29aa4846f813fc2d00e85a5627934d7ddf5eb2
xanifaibraahim <xaniifoosman@gmail.com>
2026-04-12 04:23:41 +0200

    Add files via upload

---
+++ jsExercises/exercise24.js
@@ -0,0 +1,17 @@
+
+
+
+//function paramater exercise if no height provided use width as alternative
+
+
+function calculateArea(width = 1, height = 1) {
+
+
+    return width * height
+
+
+
+}
+
+
+console.log(calculateArea(24))
